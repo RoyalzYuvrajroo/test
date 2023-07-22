306bfc40298e6b1ecb2cfa066ef92757c8b53864
RoyalzYuvrajroo <115344543+RoyalzYuvrajroo@users.noreply.github.com>
2023-07-22 11:34:46 +1000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,209 @@
+'use strict';
+
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+/**
+ * NAVBAR TOGGLE FOR MOBILE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+/**
+ * HEADER
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * SLIDER
+ */
+
+const sliders = document.querySelectorAll("[data-slider]");
+
+const initSlider = function(currentSlider) {
+
+  const sldierContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+
+  let currentSlidePos = 0;
+
+  const moveSliderItem = function () {
+    sldierContainer.style.transform = `translateX(-${sldierContainer.children[currentSlidePos].offsetLeft}px)`;
+  }
+
+  /**
+   * NEXT SLIDE
+   */
+
+  const slideNext = function () {
+    const slideEnd = currentSlidePos >= sldierContainer.childElementCount - 1;
+
+    if (slideEnd) {
+      currentSlidePos = 0;
+    } else {
+      currentSlidePos++;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderNextBtn.addEventListener("click", slideNext);
+
+  /**
+   * PREVIOUS SLIDE
+   */
+
+   const slidePrev = function () {
+
+    if (currentSlidePos <= 0) {
+      currentSlidePos = sldierContainer.childElementCount - 1;
+    } else {
+      currentSlidePos--;
+    }
+
+    moveSliderItem();
+  }
+
+  sliderPrevBtn.addEventListener("click", slidePrev);
+
+  const dontHaveExtraItem = sldierContainer.childElementCount <= 1;
+  if (dontHaveExtraItem) {
+    sliderNextBtn.style.display = "none";
+    sliderPrevBtn.style.display = "none";
+  }
+
+}
+
+for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
+
+
+
+/**
+ * ACCORDION
+ */
+
+const accordions = document.querySelectorAll("[data-accordion]");
+
+let lastActiveAccordion = accordions[0];
+
+const initAccordion = function (currentAccordion) {
+
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
+
+  const expandAccordion = function () {
+    if (lastActiveAccordion && lastActiveAccordion !== currentAccordion) {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+
+    currentAccordion.classList.toggle("expanded");
+
+    lastActiveAccordion = currentAccordion;
+  }
+
+  accordionBtn.addEventListener("click", expandAccordion);
+
+}
+
+for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
+
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const contactForm = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+const successMsg = document.getElementById('success-msg');
+const submitBtn = document.getElementById('submit');
+  
+const validate = (e) => {
+  e.preventDefault();
+ 
+  if (name.value.length < 3) {
+    errorElement.innerHTML = 'Your name should be at least 3 characters long.';
+    return false;
+  } 
+  
+  if (!(email.value.includes('.') && (email.value.includes('@')))) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  } 
+
+  if (!emailIsValid(email.value)) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  }
+
+  if (message.value.length < 15) {
+    errorElement.innerHTML = 'Please write a longer message.';
+    return false;
+  }
+
+  errorElement.innerHTML = '';
+  successMsg.innerHTML = 'Thank you! I will get back to you as soon as possible.'; 
+
+  e.preventDefault();
+  setTimeout(function () {
+    successMsg.innerHTML = '';
+    document.getElementById('contact-form').reset();
+  }, 6000);
+
+  return true;
+
+}
+
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+submitBtn.addEventListener('click', validate);
